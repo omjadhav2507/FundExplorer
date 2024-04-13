4a2def487c194574d9fda5324b921c7b62b3db5c
Om Jadhav <58546061+omjadhav2507@users.noreply.github.com>
2024-04-14 00:11:13 +0530

    Initial commit

--- src/pages/Tracker.jsx
+++ src/pages/Tracker.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function Tracker() {
   return (
-    <div>Tracker</div>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold mb-8">Fund Tracker</h1>
+      <div className="w-3/4 md:w-1/4">
+        <img
+          src="https://img.freepik.com/free-vector/investment-data-concept-illustration_114360-5159.jpg?t=st=1713033241~exp=1713036841~hmac=0467a4c6b8953b227b166a2a8a69519ac51ad272b41be4d553b5b84bc9f06253&w=740"
+          alt="Investment Data"
+          className="w-full rounded-lg shadow-lg"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Tracker
+export default Tracker;
